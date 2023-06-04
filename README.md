af8b791045b102eefced41040afcbd9fb569822e
surteq <91259066+surteq@users.noreply.github.com>
2023-06-04 16:32:34 +0200

    lab2 changes
    * new project structure
    
    * makefile created
    
    * fixed makefile
    
    * app.py and makefile changed
    
    * modified readme
    
    * requirements fixed
    
    * app fixed

---
+++ README.md
@@ -0,0 +1,6 @@
+Jak zainstalować zależności i uruchomić aplikację:
+
+1. Zainstaluj python, pylint, curl.
+2. Zainstalować pip i flask.
+3. Żeby odpalić program trzeba zainstalować 'requirements', przy użyciu 'make install'.
+4. Żeby uruchomić program trzeba użyć 'make run'.
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name="jan"):
+    return render_template('hello.html', name=name)
